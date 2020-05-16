57c5a4766379a0ff87a54cc36c71e2b8fc6db63b
chenkang98 <58716233+chenkang98@users.noreply.github.com>
2020-05-16 15:55:56 +0800

    Add files via upload
    ck

---
+++ 前端页面布局/js/test.js
@@ -0,0 +1,163 @@
+$(document).ready(function () {
+ 
+    // @open="handleOpen" @close="handleClose"
+ 
+    
+ $("#btn").click(function (e) {
+    sessionStorage.setItem("name", "ck");
+     sessionStorage.setItem("token", "token");
+    window.location.href = "test1.html"
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+});
+/*
+ < td >
+     <
+     p class = "serviceidinf" >
+     <
+     span >
+     软件售后服务编号： <
+     /span> <
+ span class = "serviceid" > {
+         {
+             serviceId[1]
+         }
+     } <
+     /span> < /
+     p > <
+     p class = "processinf" >
+     <
+     span >
+     当前状态： <
+     /span> <
+ span class = "process" > {
+         {
+             serviceState[1]
+         }
+     } <
+     /span> < /
+     p > <
+     p class = "applicantinf" >
+     <
+     span > 申请人： < /span>{{Applicants[1]}}<span class="applicant"></span > < /p> <
+ p class = "reasoninf" > < span > 申请原因： < /span><span class="reason">{{Application_reason[1]}} < /
+     span > < /p > <
+     p class = "solutioninf" >
+     <
+     span > 处理方式： < /span> <select class="solution "> <
+ option value = "反馈消息" > 反馈消息 < /option> <
+ option value = "换人" > 换人 < /option> <
+ option value = "改变状态" > 改变状态 < /option>
+
+     <
+     /select></p >
+     <
+     p class = "commit" > < button class = "commitbtn" > 提交 < /button></p >
+     <
+     /td> <
+ td >
+     <
+     p class = "serviceidinf" >
+     <
+     span >
+     软件售后服务编号： <
+     /span> <
+ span class = "serviceid" > {
+         {
+             serviceId[2]
+         }
+     } <
+     /span> < /
+     p > <
+     p class = "processinf" >
+     <
+     span >
+     当前状态： <
+     /span> <
+ span class = "process" > {
+         {
+             serviceState[2]
+         }
+     } <
+     /span> < /
+     p > <
+     p class = "applicantinf" >
+     <
+     span > 申请人： < /span>{{Applicants[2]}}<span class="applicant"></span > < /p> <
+ p class = "reasoninf" > < span > 申请原因： < /span><span class="reason">{{Application_reason[2]}} < /
+     span > < /p > <
+     p class = "solutioninf" >
+     <
+     span > 处理方式： < /span> <select class="solution "> <
+ option value = "反馈消息" > 反馈消息 < /option> <
+ option value = "换人" > 换人 < /option> <
+ option value = "改变状态" > 改变状态 < /option>
+
+     <
+     /select></p >
+     <
+     p class = "commit" > < button class = "commitbtn" > 提交 < /button></p >
+     <
+     /td> <
+ td >
+     <
+     p class = "serviceidinf" >
+     <
+     span >
+     软件售后服务编号： <
+     /span> <
+ span class = "serviceid" > {
+         {
+             serviceId[3]
+         }
+     } <
+     /span> < /
+     p > <
+     p class = "processinf" >
+     <
+     span >
+     当前状态： <
+     /span> <
+ span class = "process" > {
+         {
+             serviceState[3]
+         }
+     } <
+     /span> < /
+     p > <
+     p class = "applicantinf" >
+     <
+     span > 申请人： < /span>{{Applicants[3]}}<span class="applicant"></span > < /p> <
+ p class = "reasoninf" > < span > 申请原因： < /span><span class="reason">{{Application_reason[3]}} < /
+     span > < /p > <
+     p class = "solutioninf" >
+     <
+     span > 处理方式： < /span> <select class="solution "> <
+ option value = "反馈消息" > 反馈消息 < /option> <
+ option value = "换人" > 换人 < /option> <
+ option value = "改变状态" > 改变状态 < /option>
+
+     <
+     /select></p >
+     <
+     p class = "commit" > < button class = "commitbtn" > 提交 < /button></p >
+     <
+     /td>
+*/
